de15e51db4d69baf1913b494832696a10858867c
Damien Jarry <damien.jarry@wengo.com>
2014-05-22 13:25:32 +0200

    Add root PHP file

---
+++ ExternalTrackingBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the ExternalTrackingBundle.
+ *
+ * (c) Damien Jarry <dev.damienjarry@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GeekyHouse\ExternalTrackingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * ExternalTrackingBundle
+ *
+ * @author Damien Jarry <dev.damienjarry@gmail.com>
+ */
+class ExternalTrackingBundle extends Bundle
+{
+}
