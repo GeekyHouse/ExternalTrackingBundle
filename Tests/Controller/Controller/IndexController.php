51f7a3059523b6fcbb23eb375da5d0340cb7b337
Damien Jarry <damien.jarry@wengo.com>
2014-05-28 17:41:07 +0200

    Use @Template for indexAction.

--- Tests/Controller/Controller/IndexController.php
+++ Tests/Controller/Controller/IndexController.php
@@ -3,10 +3,14 @@
 namespace GeekyHouse\ExternalTrackingBundle\Tests\Controller\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 class IndexController extends Controller
 {
 
+    /**
+     * @Template("@test_views/index.html.twig")
+     */
     public function indexAction($name)
     {
         return array('name' => $name);
