e905b4b542e7dd38eb220abb9e9ff80f59984c94
Damien Jarry <damien.jarry@wengo.com>
2014-05-28 15:22:48 +0200

    Remove jQuery dependence and split methods.

--- Twig/Extension/ExternalTrackingExtension.php
+++ Twig/Extension/ExternalTrackingExtension.php
@@ -22,7 +22,7 @@ use Twig_Extension;
  * @author  Damien Jarry
  * @version 1.0
  * @uses    GeekyHouse\ExternalTrackingBundle\Service\ExternalTrackingManager
- * @uses    Twig_Extension;
+ * @uses    Twig_Extension
  */
 class ExternalTrackingExtension extends Twig_Extension
 {
@@ -75,15 +75,12 @@ class ExternalTrackingExtension extends Twig_Extension
         $delayIsOk      = $delay && is_numeric($delay);
 
         // Get trackers available for writing
-        $trackersOutput = $this->ExternalTrackingManager->getTrackersHTML();
+        $trackersOutput = $this->cleanJavascriptString(
+                            $this->ExternalTrackingManager->getTrackersHTML()
+                        );
+
         // Remove trackers from session
         $this->ExternalTrackingManager->removeSessionTrackers();
-        // Escape JS chars
-        $trackersOutput = preg_replace_callback('~([\\\\]*)(["]+)~i', array($this, 'escapeDoubleQuoteJS'), $trackersOutput);
-        // Escape JS close tag
-        $trackersOutput = str_replace('</script>', '<\/script>', $trackersOutput);
-        // remove linebreaks
-        $trackersOutput = preg_replace('~\r|\n|\r\n~i', '', $trackersOutput);
 
         if(!empty($trackersOutput)) {
             // Open script
@@ -92,8 +89,9 @@ class ExternalTrackingExtension extends Twig_Extension
 
             // Open documentReady
             if($documentReady) {
-                $output .= 'var DOMReady = function(a,b,c){b=document,c='addEventListener';b[c]?b[c]('DOMContentLoaded',a):window.attachEvent('onload',a)}
-			    DOMReady(function(){';
+                $output .= '
+                    var DOMReady = function(a,b,c){b=document,c=\'addEventListener\';b[c]?b[c](\'DOMContentLoaded\',a):window.attachEvent(\'onload\',a)}
+                    DOMReady(function(){';
             }
 
             // Open timeout
@@ -101,7 +99,14 @@ class ExternalTrackingExtension extends Twig_Extension
                 $output .= 'setTimeout(function(){';
             }
 
-            $output .= '$("#external-trackers-place").append("'.$trackersOutput.'");';
+            $output .= '
+                var ExternalTrackerPlace = document.getElementById("external-trackers-place");
+                    ExternalTrackerPlace.innerHTML = "'.$trackersOutput.'";
+                var scripts = ExternalTrackerPlace.getElementsByTagName(\'script\');
+                for(var i in scripts) {
+                    eval(scripts[i].innerHTML);
+                }
+            ';
 
             // Close timeout
             if($delayIsOk) {
@@ -118,6 +123,23 @@ class ExternalTrackingExtension extends Twig_Extension
         }
 
         return $output;
+    }
+
+    /**
+     * This method alters JS string to be used by this extension
+     * @param  string $string, The original Javascript string
+     * @return string
+     */
+    private function cleanJavascriptString($string)
+    {
+        // Escape JS chars
+        $string = preg_replace_callback('~([\\\\]*)(["]+)~i', array($this, 'escapeDoubleQuoteJS'), $string);
+        // Escape JS close tag
+        $string = str_replace('</script>', '<\/script>', $string);
+        // remove linebreaks
+        $string = preg_replace('~\r|\n|\r\n~i', '', $string);
+
+        return $string;
     }
 
     /**
